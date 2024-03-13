5b66be106c4988f94b9283f647bc56f3e7926087
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2024-03-13 20:14:22 +0100

    adding project

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import skillChart from "../../public/skills.svg"
 import tech from "../../public/tech.png"
 import easy from "../../public/easy.png"
+import phone from "../../public/phone.png"
+import Link from 'next/link'
+import { CgLink } from "react-icons/cg";
+import { FiGithub } from "react-icons/fi";
 
 const page = () => {
   return (
@@ -29,17 +33,45 @@ const page = () => {
           </p>
 
 
-        <div className="grid px-44">
-          <div className='justify-self-start  w-[500px] h-fit rounded-md  border-4 border-grey-600 mt-16'>
+        <div className="grid md:px-44 p-8">
+          <div className='md:justify-self-start justify-self-center  md:w-[500px] w-fit h-fit rounded-md  mt-16'>
             <Image src={easy} alt='skillChart' width={500}
               height={500} />
+            <div className="flex flex-col">
+              <p className="font-black text-2xl text-[#363636] mt-3"> EasyCommun</p>
+              <p className="text-xs text-[#898989] mt-2 "> A Next.js-based platform for seamless communication between teachers and students. With features like performance tracking and file sharing, Easycommu revolutionizes educational interaction. Teachers engage students effortlessly, while students access resources and updates conveniently. Easycommu redefines education communication, fostering collaboration and efficiency. Experience the future of learning with Easycommu.</p>
+            </div>
+            <div className="flex mt-2 gap-4 items-center">
+              <CgLink size={20}/>
+              <FiGithub size={18}/>
+            </div>
           </div>
-          <div className="justify-self-end   w-[500px] h-fit  rounded-md border-4  border-grey-600  mt-16 ">
+          <div className="md:justify-self-end  justify-self-center   md:w-[500px] w-fit h-fit rounded-md mt-16 ">
             <Image src={tech} alt='skillChart' width={500}
               height={500} />
+            <div className="flex flex-col">
+              <p className="font-black text-2xl text-[#363636] mt-3">Tech1M</p>
+              <p className="text-xs text-[#898989] mt-2 ">  A Talent Acquisition Platform revolutionizing the way businesses find, evaluate, hire, and manage global payroll. Powered by React, Next.js, and OpenAI, Tech1M develops a pipeline of tech talents with cutting-edge technology. Seamlessly connect with top talent worldwide and streamline your hiring process with Tech1M.</p>
+            </div>
+            <div className="flex mt-2 gap-4 items-center">
+              <Link href="https://tech1m.com/" target='blank'><CgLink size={20} /></Link>           
+            </div>
           </div>
-          <div className='justify-self-start  w-[500px] h-fit rounded-md border-4 border-grey-600 mt-16'></div>
 
+          <div className='md:justify-self-start justify-self-center    md:w-[500px] w-fit h-fit rounded-md mt-16'>
+            <Image src={phone} alt='skillChart' width={500}
+              height={500} />
+            <div className="flex flex-col">
+              <p className="font-black text-2xl text-[#363636] mt-3">Mstore</p>
+              <p className="text-xs text-[#898989] mt-2 ">Mstore is a revolutionary two-in-one platform that combines the features of an ecommerce marketplace and a freelancing platform. It allows buyers to access sellers storefronts to buy and sell goods and services, creating a seamless experience for both transactions and freelance collaborations.</p>
+
+            </div>
+            <div className="flex mt-2 gap-4 items-center">
+              <Link href=" https://mstore-peach.vercel.app/" target='blank'><CgLink size={20} /></Link>
+            </div>
+                </div>
+        
+        
         </div>
         
       </div>
